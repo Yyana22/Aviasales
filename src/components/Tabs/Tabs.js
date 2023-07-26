1b41ac7f194453a52231c5edc19276ef3b39d303
Yana~ <sharik.spartak2@mail.ru>
2023-07-26 20:28:51 +0300

    correct btn

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -8,23 +8,44 @@ const Tabs = ({ state, setLowCost, setFaster, setOptimal }) => {
   return (
     <div className={classes['filters-sales']}>
       <ul className={classes['filters-sales-list']}>
-        <li
-          className={state.tabs.lowCost ? classes['filters-sales-item__active'] : classes['filters-sales-item']}
-          onClick={() => setLowCost()}
-        >
-          САМЫЙ ДЕШЕВЫЙ
+        <li className={classes['filters-sales-item']}>
+          <button
+            className={
+              state.tabs.lowCost
+                ? `${classes['filters-sales-button__active']} ${classes['one-btn']}`
+                : `${classes['filters-sales-button']} ${classes['one-btn']}`
+            }
+            disabled={state.tabs.lowCost}
+            onClick={() => setLowCost()}
+          >
+            САМЫЙ ДЕШЕВЫЙ
+          </button>
         </li>
-        <li
-          className={state.tabs.faster ? classes['filters-sales-item__active'] : classes['filters-sales-item']}
-          onClick={() => setFaster()}
-        >
-          САМЫЙ БЫСТРЫЙ
+        <li className={classes['filters-sales-item']}>
+          <button
+            className={
+              state.tabs.faster
+                ? `${classes['filters-sales-button__active']} ${classes['two-btn']}`
+                : `${classes['filters-sales-button']} ${classes['two-btn']}`
+            }
+            disabled={state.tabs.faster}
+            onClick={() => setFaster()}
+          >
+            САМЫЙ БЫСТРЫЙ
+          </button>
         </li>
-        <li
-          className={state.tabs.optimal ? classes['filters-sales-item__active'] : classes['filters-sales-item']}
-          onClick={() => setOptimal()}
-        >
-          ОПТИМАЛЬНЫЙ
+        <li className={classes['filters-sales-item']}>
+          <button
+            className={
+              state.tabs.optimal
+                ? `${classes['filters-sales-button__active']} ${classes['three-btn']}`
+                : `${classes['filters-sales-button']} ${classes['three-btn']}`
+            }
+            // disabled={state.tabs.optimal}
+            onClick={() => setOptimal()}
+          >
+            ОПТИМАЛЬНЫЙ
+          </button>
         </li>
       </ul>
     </div>
